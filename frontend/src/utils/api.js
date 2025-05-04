77425cb2a13e29b32c34f3068a035438cdb8005b
Avinash <avinashviolin4@gmail.com>
2025-05-04 20:23:43 +0530

    axios connection

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Adjust the URL as needed
+  baseURL: "https://bus-dwmy.onrender.com", // Adjust the URL as needed
   withCredentials: true, // for cookies
 });
 
